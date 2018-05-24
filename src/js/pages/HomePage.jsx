33dec3e529d66c8d1488b945d64324a78a9c2a19
Mikalojus Galminas <mikalojus.galminas@geniussports.com>
2018-05-24 13:17:56 +0300

    initial commit

---
+++ src/js/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SampleComponent from '../components/SampleComponent';
+
+const HomePage = () => (
+    <div>
+        <SampleComponent text="123" />
+    </div>
+);
+
+export default HomePage;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/index.jsx',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+    ],
+};
